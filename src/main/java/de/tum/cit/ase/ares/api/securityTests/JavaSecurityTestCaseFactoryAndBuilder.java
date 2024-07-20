573bbfa467f13aadbac013a191932ac46ac150eb
Markus Paulsen <paulsenm@in.tum.de>
2024-07-20 20:47:10 +0200

    Commented JavaSecurityTestCaseFactoryAndBuilder.java and SecurityTestCaseAbstractFactoryAndBuilder.java

--- src/main/java/de/tum/cit/ase/ares/api/securityTests/JavaSecurityTestCaseFactoryAndBuilder.java
+++ src/main/java/de/tum/cit/ase/ares/api/securityTests/JavaSecurityTestCaseFactoryAndBuilder.java
@@ -10,18 +10,46 @@ import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Produces and executes or writes security test cases in the Java programming language
+ * and the concrete factory of the abstract factory design pattern
+ * as well as the concrete builder of the builder design pattern.
+ *
+ * @see <a href="https://refactoring.guru/design-patterns/abstract-factory">Abstract Factory Design Pattern</a>
+ * @see <a href="https://refactoring.guru/design-patterns/builder">Builder Design Pattern</a>
+ */
 public class JavaSecurityTestCaseFactoryAndBuilder implements SecurityTestCaseAbstractFactoryAndBuilder {
 
+    /**
+     * Security policy for the security test cases
+     */
     SecurityPolicy securityPolicy;
+    /**
+     * List of Java architecture test cases
+     */
     List<JavaArchitectureTestCase> javaArchitectureTestCases;
+    /**
+     * List of Java aspect configurations
+     */
     List<JavaAspectConfiguration> javaAspectConfigurations;
 
+    /**
+     * Constructor for the Java security test case factory and builder.
+     *
+     * @param securityPolicy Security policy for the security test cases
+     */
     public JavaSecurityTestCaseFactoryAndBuilder(SecurityPolicy securityPolicy) {
         this.securityPolicy = securityPolicy;
         javaArchitectureTestCases = new ArrayList<>();
         javaAspectConfigurations = new ArrayList<>();
     }
 
+    /**
+     * Writes the security test cases to files in the Java programming language.
+     *
+     * @param path Path to the directory where the files should be written to
+     * @return List of paths of the written files
+     */
     @Override
     public List<Path> writeTestCasesToFiles(Path path) {
         try {
@@ -62,6 +90,9 @@ public class JavaSecurityTestCaseFactoryAndBuilder implements SecurityTestCaseAb
 
     }
 
+    /**
+     * Runs the security test cases in the Java programming language.
+     */
     @Override
     public void runSecurityTestCases() {
         javaArchitectureTestCases.forEach(JavaArchitectureTestCase::runArchitectureTestCase);
--- src/main/java/de/tum/cit/ase/ares/api/securityTests/SecurityTestCaseAbstractFactoryAndBuilder.java
+++ src/main/java/de/tum/cit/ase/ares/api/securityTests/SecurityTestCaseAbstractFactoryAndBuilder.java
@@ -3,9 +3,26 @@ package de.tum.cit.ase.ares.api.securityTests;
 import java.nio.file.Path;
 import java.util.List;
 
+/**
+ * Produces and executes or writes security test cases in any programming language
+ * and the abstract factory of the abstract factory design pattern
+ * as well as the abstract builder of the builder design pattern.
+ *
+ * @see <a href="https://refactoring.guru/design-patterns/abstract-factory">Abstract Factory Design Pattern</a>
+ * @see <a href="https://refactoring.guru/design-patterns/builder">Builder Design Pattern</a>
+ */
 public interface SecurityTestCaseAbstractFactoryAndBuilder {
 
+    /**
+     * Writes the security test cases to files in any programming language.
+     *
+     * @param path Path to the directory where the files should be written to
+     * @return List of paths of the written files
+     */
     List<Path> writeTestCasesToFiles(Path path);
 
+    /**
+     * Runs the security test cases in any programming language.
+     */
     void runSecurityTestCases();
 }
