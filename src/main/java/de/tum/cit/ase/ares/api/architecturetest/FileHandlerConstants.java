855a6917db549a631f48086f57b5c45cab7f7a0b
sarps <sarpsahinalp.official@gmail.com>
2024-07-21 16:50:51 +0200

    Integrated ArchUnit tests within the JavaSecurityTestCaseFactoryAndBuilder and implemented an efficient way to store and utilize pre-compile test cases as well as post compile ones.

---
+++ src/main/java/de/tum/cit/ase/ares/api/architecturetest/FileHandlerConstants.java
@@ -0,0 +1,14 @@
+package de.tum.cit.ase.ares.api.architecturetest;
+
+/**
+ * Constants for the path of the files used in the ArchitectureTestCaseStorage
+ */
+public class FileHandlerConstants {
+
+    private FileHandlerConstants() {
+        throw new IllegalArgumentException("Utility class");
+    }
+
+    public static final String JAVA_FILESYSTEM_INTERACTION_METHODS = "src/main/resources/archunit/files/java/file-system-access-methods.txt";
+    public static final String JAVA_FILESYSTEM_INTERACTION_CONTENT = "src/main/resources/archunit/files/java/file-system-arch-rule.txt";
+}
---
+++ src/main/java/de/tum/cit/ase/ares/api/architecturetest/java/postcompile/SecurityRules.java
@@ -0,0 +1,34 @@
+package de.tum.cit.ase.ares.api.architecturetest.java.postcompile;
+
+import com.tngtech.archunit.base.DescribedPredicate;
+import com.tngtech.archunit.core.domain.JavaAccess;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import de.tum.cit.ase.ares.api.architecturetest.ArchitectureTestCaseStorage;
+
+import java.util.Objects;
+
+import static de.tum.cit.ase.ares.api.architecturetest.java.JavaSupportedArchitectureTestCase.FILESYSTEM_INTERACTION;
+
+/**
+ * Security rules for the Java programming language.
+ */
+public class SecurityRules {
+
+    private SecurityRules() {
+        throw new IllegalArgumentException("Utility class");
+    }
+
+    /**
+     * Rule for file system interaction.
+     */
+    public static final ArchRule FILE_SYSTEM_INTERACTION_RULE = ArchRuleDefinition.noClasses()
+            .should(new TransitivelyAccessesMethodsCondition(new DescribedPredicate<>("accesses file system") {
+                @Override
+                public boolean test(JavaAccess<?> javaAccess) {
+                    return Objects.requireNonNull(ArchitectureTestCaseStorage
+                                    .FORBIDDEN_METHODS_FOR_SUPPORTED_ARCHITECTURAL_TEST_CASE.build().get(FILESYSTEM_INTERACTION.name()))
+                            .contains(javaAccess.getTarget().getFullName());
+                }
+            }));
+}
