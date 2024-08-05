afa652e10aab289fda2586ad0499cf96e6b225a1
sarps <sarpsahinalp.official@gmail.com>
2024-08-05 03:11:22 +0200

    Changed properties to not fail on empty should clause

--- src/main/java/de/tum/cit/ase/ares/api/architecturetest/java/postcompile/CustomClassResolver.java
+++ src/main/java/de/tum/cit/ase/ares/api/architecturetest/java/postcompile/CustomClassResolver.java
@@ -3,7 +3,6 @@ package de.tum.cit.ase.ares.api.architecturetest.java.postcompile;
 import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
-import com.tngtech.archunit.core.importer.ImportOption;
 
 import java.util.Optional;
 
@@ -11,7 +10,6 @@ import java.util.Optional;
  * Custom class resolver to resolve classes that are outside classpath to be able to analyze them transitively.
  */
 public class CustomClassResolver {
-
     /**
      * Class file importer to import the class files.
      * This is used to import the class files from the URL.
@@ -19,9 +17,10 @@ public class CustomClassResolver {
     private final JavaClasses allClasses;
 
     public CustomClassResolver() {
+        // We need to import all classes to be able to resolve them later.
+        // TODO: We definitely need to improve this. We should not import all classes as it is not memory efficient.
+        // https://www.javadoc.io/doc/com.tngtech.archunit/archunit/0.10.2/com/tngtech/archunit/core/importer/ClassFileImporter.html
         allClasses = new ClassFileImporter()
-                .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
-                .withImportOption(location -> location.toString().contains("jrt:/"))
                 .importClasspath();
     }
 
