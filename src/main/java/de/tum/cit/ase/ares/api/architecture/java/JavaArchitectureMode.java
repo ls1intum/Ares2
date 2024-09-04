5640be3fd2acb01123b12ad3fcb2dfce58ab4eda
Markus Paulsen <paulsenm@in.tum.de>
2024-09-04 22:53:05 +0200

    Added last missing files.

--- src/main/java/de/tum/cit/ase/ares/api/architecture/java/JavaArchitectureMode.java
+++ src/main/java/de/tum/cit/ase/ares/api/architecture/java/JavaArchitectureMode.java
@@ -13,10 +13,21 @@ public enum JavaArchitectureMode {
     public List<Path> filesToCopy() {
         return (switch (this) {
             case ARCHUNIT -> Stream.of(
+                    new String[]{"templates", "architecture", "java", "archunit", "methods", "file-system-access-methods.txt"},
+                    new String[]{"templates", "architecture", "java", "archunit", "methods", "jvm-termination-methods.txt"},
+                    new String[]{"templates", "architecture", "java", "archunit", "methods", "network-access-methods.txt"},
+                    new String[]{"templates", "architecture", "java", "archunit", "methods", "reflection-methods.txt"},
                     new String[]{"templates", "architecture", "java", "archunit", "postcompile", "CustomClassResolver.java"},
                     new String[]{"templates", "architecture", "java", "archunit", "postcompile", "JavaArchitectureTestCaseCollection.java"},
                     new String[]{"templates", "architecture", "java", "archunit", "postcompile", "TransitivelyAccessesMethodsCondition.java"},
-                    new String[]{"templates", "architecture", "java", "archunit", "FileHandlerConstants.java"}
+                    new String[]{"templates", "architecture", "java", "archunit", "rules", "COMMAND_EXECUTION.txt"},
+                    new String[]{"templates", "architecture", "java", "archunit", "rules", "FILESYSTEM_INTERACTION.txt"},
+                    new String[]{"templates", "architecture", "java", "archunit", "rules", "NETWORK_CONNECTION.txt"},
+                    new String[]{"templates", "architecture", "java", "archunit", "rules", "PACKAGE_IMPORT.txt"},
+                    new String[]{"templates", "architecture", "java", "archunit", "rules", "THREAD_CREATION.txt"},
+                    new String[]{"templates", "architecture", "java", "archunit", "archunit.properties"},
+                    new String[]{"templates", "architecture", "java", "archunit", "FileHandlerConstants.java"},
+                    new String[]{"templates", "architecture", "java", "archunit", "JavaArchUnitTestCaseSupported.java"}
             );
         }).map(FileTools::resolveOnResources).toList();
     }
@@ -24,8 +35,19 @@ public enum JavaArchitectureMode {
     public List<String[]> fileValues(String packageName) {
         return (switch (this) {
             case ARCHUNIT -> Stream.of(
+                    new String[]{},
+                    new String[]{},
+                    new String[]{},
+                    new String[]{},
                     FileTools.generatePackageNameArray(packageName, 1),
                     FileTools.generatePackageNameArray(packageName, 6),
+                    FileTools.generatePackageNameArray(packageName, 1),
+                    new String[]{},
+                    new String[]{},
+                    new String[]{},
+                    new String[]{},
+                    new String[]{},
+                    new String[]{},
                     FileTools.generatePackageNameArray(packageName, 1),
                     FileTools.generatePackageNameArray(packageName, 1)
             );
@@ -35,30 +57,44 @@ public enum JavaArchitectureMode {
     public List<Path> targetsToCopyTo(Path projectPath, String packageName) {
         return (switch (this) {
             case ARCHUNIT -> Stream.of(
+                    new String[]{"architecture", "java", "archunit", "methods", "file-system-access-methods.txt"},
+                    new String[]{"architecture", "java", "archunit", "methods", "jvm-termination-methods.txt"},
+                    new String[]{"architecture", "java", "archunit", "methods", "network-access-methods.txt"},
+                    new String[]{"architecture", "java", "archunit", "methods", "reflection-methods.txt"},
                     new String[]{"architecture", "java", "archunit", "postcompile", "CustomClassResolver.java"},
                     new String[]{"architecture", "java", "archunit", "postcompile", "JavaArchitectureTestCaseCollection.java"},
                     new String[]{"architecture", "java", "archunit", "postcompile", "TransitivelyAccessesMethodsCondition.java"},
-                    new String[]{"architecture", "java", "archunit", "FileHandlerConstants.java"}
+                    new String[]{"architecture", "java", "archunit", "rules", "COMMAND_EXECUTION.txt"},
+                    new String[]{"architecture", "java", "archunit", "rules", "FILESYSTEM_INTERACTION.txt"},
+                    new String[]{"architecture", "java", "archunit", "rules", "NETWORK_CONNECTION.txt"},
+                    new String[]{"architecture", "java", "archunit", "rules", "PACKAGE_IMPORT.txt"},
+                    new String[]{"architecture", "java", "archunit", "rules", "THREAD_CREATION.txt"},
+                    new String[]{"architecture", "java", "archunit", "archunit.properties"},
+                    new String[]{"architecture", "java", "archunit", "FileHandlerConstants.java"},
+                    new String[]{"architecture", "java", "archunit", "JavaArchUnitTestCaseSupported.java"}
             );
         }).map(pathParticles -> FileTools.resolveOnTests(projectPath, packageName, pathParticles)).toList();
     }
 
     public Path threePartedFileHeader() {
         return FileTools.resolveOnResources(switch (this) {
-            case ARCHUNIT -> new String[]{"templates", "architecture", "java", "archunit", "JavaArchitectureTestCaseCollectionHeader.txt"};
+            case ARCHUNIT ->
+                    new String[]{"templates", "architecture", "java", "archunit", "JavaArchitectureTestCaseCollectionHeader.txt"};
         });
     }
 
     @SuppressWarnings("unchecked")
     public String threePartedFileBody(List<?> testCases) {
         return switch (this) {
-            case ARCHUNIT -> String.join("\n", ((List<JavaArchUnitSecurityTestCase>) testCases).stream().map(JavaArchUnitSecurityTestCase::writeArchitectureTestCase).toList());
+            case ARCHUNIT ->
+                    String.join("\n", ((List<JavaArchUnitSecurityTestCase>) testCases).stream().map(JavaArchUnitSecurityTestCase::writeArchitectureTestCase).toList());
         };
     }
 
     public Path threePartedFileFooter() {
         return FileTools.resolveOnResources(switch (this) {
-            case ARCHUNIT -> new String[]{"templates", "architecture", "java", "archunit", "JavaArchitectureTestCaseCollectionFooter.txt"};
+            case ARCHUNIT ->
+                    new String[]{"templates", "architecture", "java", "archunit", "JavaArchitectureTestCaseCollectionFooter.txt"};
         });
     }
 
