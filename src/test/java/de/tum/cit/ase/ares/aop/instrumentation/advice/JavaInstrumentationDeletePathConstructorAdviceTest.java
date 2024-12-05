747bc16038cc5daf15acb767900d13f99298b64a
Markus Paulsen <39456125+MarkusPaulsen@users.noreply.github.com>
2024-12-05 18:47:57 +0100

    Update src/test/java/de/tum/cit/ase/ares/aop/instrumentation/advice/JavaInstrumentationDeletePathConstructorAdviceTest.java
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/test/java/de/tum/cit/ase/ares/aop/instrumentation/advice/JavaInstrumentationDeletePathConstructorAdviceTest.java
+++ src/test/java/de/tum/cit/ase/ares/aop/instrumentation/advice/JavaInstrumentationDeletePathConstructorAdviceTest.java
@@ -9,31 +9,53 @@ import static org.mockito.Mockito.*;
 
 class JavaInstrumentationDeletePathConstructorAdviceTest {
 
+    private static final String OPERATION = "delete";
+    private static final String CLASS_NAME = "de.tum.cit.ase.ares.api.aop.java.instrumentation.advice.JavaInstrumentationDeletePathConstructorAdvice";
+    private static final String METHOD_NAME = "<init>";
+    private static final Object[] EMPTY_ARGS = new Object[0];
+
     @Test
-    void testOnEnter() {
+    void testOnEnterVerifiesFileSystemInteractionForDelete() {
+        // Arrange
+        String param1 = "param1";
+        String param2 = "param2";
+        Object[] params = new Object[]{param1, param2};
+
         try (MockedStatic<JavaInstrumentationAdviceToolbox> mockedToolbox = mockStatic(JavaInstrumentationAdviceToolbox.class)) {
             mockedToolbox.when(() -> JavaInstrumentationAdviceToolbox.checkFileSystemInteraction(
-                    "delete",
-                    "de.tum.cit.ase.ares.api.aop.java.instrumentation.advice.JavaInstrumentationDeletePathConstructorAdvice",
-                    "<init>",
+                    OPERATION,
+                    CLASS_NAME,
+                    METHOD_NAME,
                     "",
-                    new Object[0],
-                    new Object[]{"param1", "param2"}
+                    EMPTY_ARGS,
+                    params
             )).thenAnswer(invocation -> null);
 
+            // Act
             JavaInstrumentationDeletePathConstructorAdvice.onEnter(
-                    "de.tum.cit.ase.ares.api.aop.java.instrumentation.advice.JavaInstrumentationDeletePathConstructorAdvice",
-                    "param1", "param2"
+                    CLASS_NAME,
+                    param1, param2
             );
 
+            // Assert
             mockedToolbox.verify(() -> JavaInstrumentationAdviceToolbox.checkFileSystemInteraction(
-                    "delete",
-                    "de.tum.cit.ase.ares.api.aop.java.instrumentation.advice.JavaInstrumentationDeletePathConstructorAdvice",
-                    "<init>",
+                    OPERATION,
+                    CLASS_NAME,
+                    METHOD_NAME,
                     "",
-                    new Object[0],
-                    new Object[]{"param1", "param2"}
+                    EMPTY_ARGS,
+                    params
             ));
         }
+    }
+
+    @Test
+    void testOnEnterWithNullParameters() {
+        // Add test case for null parameters
+    }
+
+    @Test
+    void testOnEnterWithEmptyParameters() {
+        // Add test case for empty parameters
     }
 }
