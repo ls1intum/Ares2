8106d09b1354305b9cbfb5c8183f9f1ed245b3d0
Ajayvir S. <ajayvirsingh11@hotmail.com>
2025-07-05 00:14:32 +0200

    fix architecture tests and overload methods to pass path

--- src/test/java/de/tum/cit/ase/ares/integration/architecture/forbidden/FileSystemAccessOverwriteTest.java
+++ src/test/java/de/tum/cit/ase/ares/integration/architecture/forbidden/FileSystemAccessOverwriteTest.java
@@ -9,11 +9,20 @@ import de.tum.cit.ase.ares.integration.testuser.subject.architectureTests.thirdp
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Path;
+
 class FileSystemAccessOverwriteTest extends SystemAccessTest {
 
-    private static final String FILES_WRITE_PATH = "test-classes/de/tum/cit/ase/ares/integration/aop/forbidden/subject/fileSystem/overwrite/filesWrite";
-    private static final String BUFFERED_WRITER_PATH = "test-classes/de/tum/cit/ase/ares/integration/aop/forbidden/subject/fileSystem/overwrite/bufferedWriter";
+
+    private static final Path NOT_TRUSTED_DIR = Path.of("src/test/java/de/tum/cit/ase/ares/integration/aop/forbidden/subject/fileSystem/overwrite/nottrusteddir");
+    private static final Path NOT_TRUSTED_GZ = Path.of(NOT_TRUSTED_DIR + "/nottrusted.txt.gz");
+    private static final Path NOT_TRUSTED_FILE_PATH = Path.of(NOT_TRUSTED_DIR + "/nottrusted.txt");
+
+
+    private static final String FILES_WRITE_PATH = "test-classes/de/tum/cit/ase/ares/integration/aop/forbidden/subject/fileSystem/overwrite/files";
+    private static final String BUFFERED_WRITER_PATH = "test-classes/de/tum/cit/ase/ares/integration/aop/forbidden/subject/fileSystem/overwrite/writer/bufferedWriter";
     private static final String THIRD_PARTY_PACKAGE_PATH = "test-classes/de/tum/cit/ase/ares/integration/aop/forbidden/subject/fileSystem/overwrite/thirdPartyPackage";
+
 
     // <editor-fold desc="accessFileSystemViaFilesWrite">
     @Test
@@ -21,23 +30,9 @@ class FileSystemAccessOverwriteTest extends SystemAccessTest {
         executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaFilesWriteMavenArchunitAspectJ");
     }
 
-    @Disabled
-    @PublicTest
-    @Policy(value = ARCHUNIT_ASPECTJ_POLICY, withinPath = FILES_WRITE_PATH)
-    void test_accessFileSystemViaFilesWriteMavenArchunitAspectJ() {
-        assertAresSecurityExceptionOverwrite(WriteFilesWriteMain::accessFileSystemViaFilesWrite, WriteFilesWriteMain.class);
-    }
-
     @Test
     void test_accessFileSystemViaFilesWriteMavenArchunitInstrumentation_test() {
         executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaFilesWriteMavenArchunitInstrumentation");
-    }
-
-    @Disabled
-    @PublicTest
-    @Policy(value = ARCHUNIT_INSTRUMENTATION_POLICY, withinPath = FILES_WRITE_PATH)
-    void test_accessFileSystemViaFilesWriteMavenArchunitInstrumentation() {
-        assertAresSecurityExceptionOverwrite(WriteFilesWriteMain::accessFileSystemViaFilesWrite, WriteFilesWriteMain.class);
     }
 
     @Test
@@ -45,23 +40,9 @@ class FileSystemAccessOverwriteTest extends SystemAccessTest {
         executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaFilesWriteMavenWalaAspectJ");
     }
 
-    @Disabled
-    @PublicTest
-    @Policy(value = WALA_ASPECTJ_POLICY, withinPath = FILES_WRITE_PATH)
-    void test_accessFileSystemViaFilesWriteMavenWalaAspectJ() {
-        assertAresSecurityExceptionOverwrite(WriteFilesWriteMain::accessFileSystemViaFilesWrite, WriteFilesWriteMain.class);
-    }
-
     @Test
     void test_accessFileSystemViaFilesWriteMavenWalaInstrumentation_test() {
         executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaFilesWriteMavenWalaInstrumentation");
-    }
-
-    @Disabled
-    @PublicTest
-    @Policy(value = WALA_INSTRUMENTATION_POLICY, withinPath = FILES_WRITE_PATH)
-    void test_accessFileSystemViaFilesWriteMavenWalaInstrumentation() {
-        assertAresSecurityExceptionOverwrite(WriteFilesWriteMain::accessFileSystemViaFilesWrite, WriteFilesWriteMain.class);
     }
     // </editor-fold>
 
@@ -69,6 +50,75 @@ class FileSystemAccessOverwriteTest extends SystemAccessTest {
     @Test
     void test_accessFileSystemViaBufferedWriterMavenArchunitAspectJ_test() {
         executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaBufferedWriterMavenArchunitAspectJ");
+    }
+
+    @Test
+    void test_accessFileSystemViaBufferedWriterMavenArchunitInstrumentation_test() {
+        executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaBufferedWriterMavenArchunitInstrumentation");
+    }
+
+    @Test
+    void test_accessFileSystemViaBufferedWriterMavenWalaAspectJ_test() {
+        executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaBufferedWriterMavenWalaAspectJ");
+    }
+
+    @Test
+    void test_accessFileSystemViaBufferedWriterMavenWalaInstrumentation_test() {
+        executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaBufferedWriterMavenWalaInstrumentation");
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="accessFileSystemViaThirdPartyPackage">
+    @Test
+    void test_accessFileSystemViaThirdPartyPackageMavenArchunitAspectJ_test() {
+        executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaThirdPartyPackageMavenArchunitAspectJ");
+    }
+
+    @Test
+    void test_accessFileSystemViaThirdPartyPackageMavenArchunitInstrumentation_test() {
+        executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaThirdPartyPackageMavenArchunitInstrumentation");
+    }
+
+    @Test
+    void test_accessFileSystemViaThirdPartyPackageMavenWalaAspectJ_test() {
+        executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaThirdPartyPackageMavenWalaAspectJ");
+    }
+
+    @Test
+    void test_accessFileSystemViaThirdPartyPackageMavenWalaInstrumentation_test() {
+        executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaThirdPartyPackageMavenWalaInstrumentation");
+    }
+    // </editor-fold>
+
+
+    // ===================== DISABLED TESTS BELOW =====================
+
+    @Disabled
+    @PublicTest
+    @Policy(value = ARCHUNIT_ASPECTJ_POLICY, withinPath = FILES_WRITE_PATH)
+    void test_accessFileSystemViaFilesWriteMavenArchunitAspectJ() {
+        assertAresSecurityExceptionOverwrite(WriteFilesWriteMain::accessFileSystemViaFilesWrite, WriteFilesWriteMain.class, NOT_TRUSTED_FILE_PATH);
+    }
+
+    @Disabled
+    @PublicTest
+    @Policy(value = ARCHUNIT_INSTRUMENTATION_POLICY, withinPath = FILES_WRITE_PATH)
+    void test_accessFileSystemViaFilesWriteMavenArchunitInstrumentation() {
+        assertAresSecurityExceptionOverwrite(WriteFilesWriteMain::accessFileSystemViaFilesWrite, WriteFilesWriteMain.class, NOT_TRUSTED_FILE_PATH);
+    }
+
+    @Disabled
+    @PublicTest
+    @Policy(value = WALA_ASPECTJ_POLICY, withinPath = FILES_WRITE_PATH)
+    void test_accessFileSystemViaFilesWriteMavenWalaAspectJ() {
+        assertAresSecurityExceptionOverwrite(WriteFilesWriteMain::accessFileSystemViaFilesWrite, WriteFilesWriteMain.class, NOT_TRUSTED_FILE_PATH);
+    }
+
+    @Disabled
+    @PublicTest
+    @Policy(value = WALA_INSTRUMENTATION_POLICY, withinPath = FILES_WRITE_PATH)
+    void test_accessFileSystemViaFilesWriteMavenWalaInstrumentation() {
+        assertAresSecurityExceptionOverwrite(WriteFilesWriteMain::accessFileSystemViaFilesWrite, WriteFilesWriteMain.class, NOT_TRUSTED_FILE_PATH);
     }
 
     @Disabled
@@ -79,22 +129,12 @@ class FileSystemAccessOverwriteTest extends SystemAccessTest {
                 BufferedWriterWriteMain.class);
     }
 
-    @Test
-    void test_accessFileSystemViaBufferedWriterMavenArchunitInstrumentation_test() {
-        executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaBufferedWriterMavenArchunitInstrumentation");
-    }
-
     @Disabled
     @PublicTest
     @Policy(value = ARCHUNIT_INSTRUMENTATION_POLICY, withinPath = BUFFERED_WRITER_PATH)
     void test_accessFileSystemViaBufferedWriterMavenArchunitInstrumentation() {
         assertAresSecurityExceptionOverwrite(BufferedWriterWriteMain::accessFileSystemViaBufferedWriter,
-                BufferedWriterWriteMain.class);
-    }
-
-    @Test
-    void test_accessFileSystemViaBufferedWriterMavenWalaAspectJ_test() {
-        executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaBufferedWriterMavenWalaAspectJ");
+                BufferedWriterWriteMain.class, NOT_TRUSTED_FILE_PATH);
     }
 
     @Disabled
@@ -102,12 +142,7 @@ class FileSystemAccessOverwriteTest extends SystemAccessTest {
     @Policy(value = WALA_ASPECTJ_POLICY, withinPath = BUFFERED_WRITER_PATH)
     void test_accessFileSystemViaBufferedWriterMavenWalaAspectJ() {
         assertAresSecurityExceptionOverwrite(BufferedWriterWriteMain::accessFileSystemViaBufferedWriter,
-                BufferedWriterWriteMain.class);
-    }
-
-    @Test
-    void test_accessFileSystemViaBufferedWriterMavenWalaInstrumentation_test() {
-        executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaBufferedWriterMavenWalaInstrumentation");
+                BufferedWriterWriteMain.class, NOT_TRUSTED_FILE_PATH);
     }
 
     @Disabled
@@ -115,15 +150,7 @@ class FileSystemAccessOverwriteTest extends SystemAccessTest {
     @Policy(value = WALA_INSTRUMENTATION_POLICY, withinPath = BUFFERED_WRITER_PATH)
     void test_accessFileSystemViaBufferedWriterMavenWalaInstrumentation() {
         assertAresSecurityExceptionOverwrite(BufferedWriterWriteMain::accessFileSystemViaBufferedWriter,
-                BufferedWriterWriteMain.class);
-    }
-    // </editor-fold>
-
-    // <editor-fold desc="accessFileSystemViaThirdPartyPackage">
-
-    @Test
-    void test_accessFileSystemViaThirdPartyPackageMavenArchunitAspectJ_test() {
-        executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaThirdPartyPackageMavenArchunitAspectJ");
+                BufferedWriterWriteMain.class, NOT_TRUSTED_FILE_PATH);
     }
 
     @Disabled
@@ -131,12 +158,7 @@ class FileSystemAccessOverwriteTest extends SystemAccessTest {
     @Policy(value = ARCHUNIT_ASPECTJ_POLICY, withinPath = THIRD_PARTY_PACKAGE_PATH)
     void test_accessFileSystemViaThirdPartyPackageMavenArchunitAspectJ() {
         assertAresSecurityExceptionOverwrite(WriteThirdPartyPackageMain::accessFileSystemViaThirdPartyPackage,
-                ThirdPartyPackagePenguin.class);
-    }
-
-    @Test
-    void test_accessFileSystemViaThirdPartyPackageMavenArchunitInstrumentation_test() {
-        executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaThirdPartyPackageMavenArchunitInstrumentation");
+                ThirdPartyPackagePenguin.class, NOT_TRUSTED_FILE_PATH);
     }
 
     @Disabled
@@ -144,12 +166,7 @@ class FileSystemAccessOverwriteTest extends SystemAccessTest {
     @Policy(value = ARCHUNIT_INSTRUMENTATION_POLICY, withinPath = THIRD_PARTY_PACKAGE_PATH)
     void test_accessFileSystemViaThirdPartyPackageMavenArchunitInstrumentation() {
         assertAresSecurityExceptionOverwrite(WriteThirdPartyPackageMain::accessFileSystemViaThirdPartyPackage,
-                ThirdPartyPackagePenguin.class);
-    }
-
-    @Test
-    void test_accessFileSystemViaThirdPartyPackageMavenWalaAspectJ_test() {
-        executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaThirdPartyPackageMavenWalaAspectJ");
+                ThirdPartyPackagePenguin.class, NOT_TRUSTED_FILE_PATH);
     }
 
     @Disabled
@@ -157,12 +174,7 @@ class FileSystemAccessOverwriteTest extends SystemAccessTest {
     @Policy(value = WALA_ASPECTJ_POLICY, withinPath = THIRD_PARTY_PACKAGE_PATH)
     void test_accessFileSystemViaThirdPartyPackageMavenWalaAspectJ() {
         assertAresSecurityExceptionOverwrite(WriteThirdPartyPackageMain::accessFileSystemViaThirdPartyPackage,
-                ThirdPartyPackagePenguin.class);
-    }
-
-    @Test
-    void test_accessFileSystemViaThirdPartyPackageMavenWalaInstrumentation_test() {
-        executeTestAndExpectSecurityException(FileSystemAccessOverwriteTest.class, "test_accessFileSystemViaThirdPartyPackageMavenWalaInstrumentation");
+                ThirdPartyPackagePenguin.class, NOT_TRUSTED_FILE_PATH);
     }
 
     @Disabled
@@ -170,7 +182,6 @@ class FileSystemAccessOverwriteTest extends SystemAccessTest {
     @Policy(value = WALA_INSTRUMENTATION_POLICY, withinPath = THIRD_PARTY_PACKAGE_PATH)
     void test_accessFileSystemViaThirdPartyPackageMavenWalaInstrumentation() {
         assertAresSecurityExceptionOverwrite(WriteThirdPartyPackageMain::accessFileSystemViaThirdPartyPackage,
-                ThirdPartyPackagePenguin.class);
+                ThirdPartyPackagePenguin.class, NOT_TRUSTED_FILE_PATH);
     }
-    // </editor-fold>
 }
