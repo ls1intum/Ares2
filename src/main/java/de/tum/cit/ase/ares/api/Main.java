cbc377507d9884f1fcd305782e0abf3a08a47a34
Ajayvir S. <ajayvirsingh11@hotmail.com>
2025-11-26 17:45:48 +0100

    fix private lambda access error

--- src/main/java/de/tum/cit/ase/ares/api/Main.java
+++ src/main/java/de/tum/cit/ase/ares/api/Main.java
@@ -7,14 +7,14 @@ import java.nio.file.Path;
 public class Main {
     public static void main(String[] args) {
         SecurityPolicyReaderAndDirector securityPolicyReaderAndDirector = new SecurityPolicyReaderAndDirector(
-                Path.of("/Users", "markuspaulsen", "Documents", "Ares2", "src", "main", "resources", "ExampleConfiguration.yaml"),
-                Path.of("/Users", "markuspaulsen", "Documents", "Ares2UI")
-//                Path.of("G:\\Programming\\ares2-test-classes\\ExampleConfiguration.yaml"),
-//                Path.of("G:\\Programming\\ares2-test-classes\\SCOREReproducibilityPackage")
+                //Path.of("/Users", "markuspaulsen", "Documents", "Ares2", "src", "main", "resources", "ExampleConfiguration.yaml"),
+                //Path.of("/Users", "markuspaulsen", "Documents", "Ares2UI")
+                Path.of("C:\\Users\\ajayv\\Desktop\\bwrap_docker\\SCOREReproducibilityPackage\\src\\test\\java\\resources\\ExampleConfiguration.yaml"),
+                Path.of("C:\\Users\\ajayv\\Desktop\\bwrap_docker\\SCOREReproducibilityPackage")
         ).createTestCases();
         securityPolicyReaderAndDirector.writeTestCases(
-                Path.of("/Users", "markuspaulsen", "Documents", "Ares2UI", "src", "test", "java")
-//                Path.of("G:\\Programming\\ares2-test-classes\\SCOREReproducibilityPackage\\src\\test\\java")
+                //Path.of("/Users", "markuspaulsen", "Documents", "Ares2UI", "src", "test", "java")
+                Path.of("C:\\Users\\ajayv\\Desktop\\bwrap_docker\\SCOREReproducibilityPackage\\src\\test\\java")
         );
     }
 }
