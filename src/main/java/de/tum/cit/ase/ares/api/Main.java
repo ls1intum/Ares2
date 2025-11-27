ebbba758fc4746bdb54e1d6dcc8502cc4a1d5972
Ajayvir S. <ajayvirsingh11@hotmail.com>
2025-11-27 12:28:05 +0100

    comment out manual test code

--- src/main/java/de/tum/cit/ase/ares/api/Main.java
+++ src/main/java/de/tum/cit/ase/ares/api/Main.java
@@ -7,14 +7,14 @@ import java.nio.file.Path;
 public class Main {
     public static void main(String[] args) {
         SecurityPolicyReaderAndDirector securityPolicyReaderAndDirector = new SecurityPolicyReaderAndDirector(
-                //Path.of("/Users", "markuspaulsen", "Documents", "Ares2", "src", "main", "resources", "ExampleConfiguration.yaml"),
-                //Path.of("/Users", "markuspaulsen", "Documents", "Ares2UI")
-                Path.of("C:\\Users\\ajayv\\Desktop\\bwrap_docker\\SCOREReproducibilityPackage\\src\\test\\java\\resources\\ExampleConfiguration.yaml"),
-                Path.of("C:\\Users\\ajayv\\Desktop\\bwrap_docker\\SCOREReproducibilityPackage")
+                Path.of("/Users", "markuspaulsen", "Documents", "Ares2", "src", "main", "resources", "ExampleConfiguration.yaml"),
+                Path.of("/Users", "markuspaulsen", "Documents", "Ares2UI")
+                //Path.of("C:\\Users\\ajayv\\Desktop\\bwrap_docker\\SCOREReproducibilityPackage\\src\\test\\java\\resources\\ExampleConfiguration.yaml"),
+                //Path.of("C:\\Users\\ajayv\\Desktop\\bwrap_docker\\SCOREReproducibilityPackage")
         ).createTestCases();
         securityPolicyReaderAndDirector.writeTestCases(
-                //Path.of("/Users", "markuspaulsen", "Documents", "Ares2UI", "src", "test", "java")
-                Path.of("C:\\Users\\ajayv\\Desktop\\bwrap_docker\\SCOREReproducibilityPackage\\src\\test\\java")
+                Path.of("/Users", "markuspaulsen", "Documents", "Ares2UI", "src", "test", "java")
+                //Path.of("C:\\Users\\ajayv\\Desktop\\bwrap_docker\\SCOREReproducibilityPackage\\src\\test\\java")
         );
     }
 }
